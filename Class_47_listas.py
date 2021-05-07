4a96d82b2c3ff9185252bbbb91784c6670751ff7
Diego Oliveira <diego.oliveira@lge.com>
2021-05-07 16:26:59 -0300

    add item in list

--- Class_47_listas.py
+++ Class_47_listas.py
@@ -4,3 +4,4 @@ print(lista[2]) #imprimir uma posição da lista
 
 print(1 in lista)  # verifica se 1 contem dentro da lisat
 print(lista[-1]) # pegar a ultima posição da lista
+print(lista[-5]) # pegar a primeira posição da lista
